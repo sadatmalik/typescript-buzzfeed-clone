75b996939a258e1331a42173b782221e3153916f
sadat.malik <sadat.malik@me.com>
2023-02-21 20:37:26 +0000

    import and use QuizData type in server.ts

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import axios, { AxiosResponse } from 'axios'
+import { QuizData } from './interfaces'
 
 const PORT = 8000
 const app = express()
@@ -13,7 +14,7 @@ app.get('/quiz-item', async (req: Request, res: Response) => {
             }
         })
         if (response.status === 200) {
-            const quizItem = await response.data.data['54a25953-f895-47a3-adc4-0f2505e2dc81']
+            const quizItem: QuizData = await response.data.data['54a25953-f895-47a3-adc4-0f2505e2dc81']
             res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
             res.send(quizItem)
         }
