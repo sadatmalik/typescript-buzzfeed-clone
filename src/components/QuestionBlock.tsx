9680a1dda2d1f6708b7a5ca4ebdd87517618a477
sadat.malik <sadat.malik@me.com>
2023-02-24 13:13:53 +0000

    populate question block buttons with quiz image data

--- src/components/QuestionBlock.tsx
+++ src/components/QuestionBlock.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React from 'react'
+import {Question} from '../../interfaces'
 
-const QuestionBlock = () => {
+const QuestionBlock = ({
+                           question
+} : {
+    question: Question
+}) => {
+
+
+
     return (
-        <div></div>
+        <button
+            className="question-block"
+            // onClick={handleClick}
+        >
+            <img src={question.image} alt={question.alt}/>
+            <h3>{question.text}</h3>
+            <p>
+                <a href={question.image}>{question.credit} </a>
+                <a href="https://www.unsplash.com">Unsplash</a>
+            </p>
+        </button>
     );
 }
 
--- src/components/QuestionsBlock.tsx
+++ src/components/QuestionsBlock.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Content } from '../../interfaces'
+import { Content, Question } from '../../interfaces'
+import QuestionBlock from './QuestionBlock'
 
 const QuestionsBlock = ({quizItem} : {
     quizItem: Content
@@ -7,6 +8,14 @@ const QuestionsBlock = ({quizItem} : {
     return (
         <>
             <h2 id={String(quizItem.id)}>{quizItem.text}</h2>
+            <div className="questions-container">
+                {quizItem?.questions.map((question: Question, _index) => (
+                    <QuestionBlock
+                        key={_index}
+                        question={question}
+                    />
+                ))}
+            </div>
         </>
     );
 }
